de46882eb61d321e1511124e5370117cb79b7e47
Vinícius Peiter <viniciuspeiter@gmail.com>
2022-01-31 09:48:59 -0300

    Add dummy calculator module to test CI

---
+++ test/multiply/test_calculator.py
@@ -0,0 +1,17 @@
+"""Test Calculator Module"""
+import unittest
+
+from parameterized import parameterized
+
+from src.multiply.calculator import Calculator
+
+
+class TestCalculator(unittest.TestCase):
+    @parameterized.expand([
+        (2, 1),
+        (2, 2),
+        (3, 5),
+        (5, 3),
+    ])
+    def test_multiply(self, operand_1, operand_2):
+        self.assertEqual(Calculator.multiply(operand_1, operand_2), operand_1 * operand_2)
